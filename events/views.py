39aebc6f2c4d4905bc59586df774939e88d1fc7e
Dimitris Gravanis <dim_grav@yahoo.gr>
2018-10-14 16:24:06 +0300

    Set basic list view in events/views

--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from events.models import Event
+from events.serializers import EventSerializer
+
+
+class EventList(ListAPIView):
+    """
+    List all registerd user accounts
+    """
+
+    permission_classes = (IsAdminUser, )
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
