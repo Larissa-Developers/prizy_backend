0da5246f07387e8ef0634fc5be2f981963906d81
Evangelos Barakos <e.mparakos@gmail.com>
2018-10-27 15:50:15 +0300

    initial bridging using ready made meetup-api package 'pip install meetup-api'

--- events/views.py
+++ events/views.py
@@ -1,5 +1,11 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import meetup.api
+import json
+
 
 # TODO: implement event-related views
 
@@ -10,5 +16,22 @@ class EventList(ListAPIView):
     """
 
     permission_classes = (IsAdminUser, )
+
+
+class Meetupcom(APIView):
+    """
+    Bridging for www.meetup.com
+    """
+
+    permission_classes = (AllowAny,)
+
+    def get(self, req):
+        try:
+            client = meetup.api.Client('648562f7e53693e31722361f144756')
+            group_info = client.GetGroup({'urlname': 'Larissa-Developers-Meetup'})
+            # use various method on client from https://meetup-api.readthedocs.io/en/latest/meetup_api.html#api-client-details
+            return Response(status=status.HTTP_200_OK, data=group_info.__dict__.values())
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
