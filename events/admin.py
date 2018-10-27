78ac06f51f02ba49d990defa707b0c0f965b8a0c
ioanna <kuriakopoulosgiorgos@gmail.com>
2018-10-27 16:22:19 +0300

    Registered the models in the admin page and configuring Media root

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Event, Fee, Venue
 # Register your models here.
+admin.site.register(Venue)
+admin.site.register(Fee)
+admin.site.register(Event)
