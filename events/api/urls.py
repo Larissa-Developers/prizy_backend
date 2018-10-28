333f8a6de16c1c1ee388091df30d699c5e8c2a25
dimgrav <dimgrav@gmail.com>
2018-10-28 11:18:28 +0200

    Moved events/api modules logic to events/ modules to make code organization simpler

--- events/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from events.api import views
-
-urlpatterns = [
-    path('', views.EventsListAPIView().as_view(), name='events'),
-]
--- events/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.generics import ListAPIView
-
-from events.models import Event
-from .serializers import  EventSerializer
-
-class EventsListAPIView(ListAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
