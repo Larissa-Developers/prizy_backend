e14189279b3faf08c06413ce15384667ca4b0848
dimgrav <dimgrav@gmail.com>
2018-10-28 11:58:17 +0200

    Remove redundant primary key declarations, adapt a PackageModel (e.g. EventVenue) naming convention for more explicit names

--- events/models.py
+++ events/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from PIL import Image
 
 
-class Venue(models.Model):
-    id = models.AutoField(primary_key=True)
-    venue_meetup_id = models.IntegerField(default=0)
+class EventVenue(models.Model):
+    """
+    Data representation of a Meetup event venue
+    """
+
+    venue_meetup_id = models.CharField(max_length=32, default='')
     country = models.CharField(max_length=32, blank=False, null=False)
     city = models.CharField(max_length=32, blank=False, null=False)
     address = models.CharField(max_length=80, blank=False, null=False)
@@ -17,8 +20,12 @@ class Venue(models.Model):
         return 'Venue : {}'.format(self.id)
 
 
-class Fee(models.Model):
-    amount = models.FloatField()
+class EventFee(models.Model):
+    """
+    Data representation of a Meetup event participation fee
+    """
+
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
     currency = models.CharField(max_length=10, default='EU')
     label = models.CharField(max_length=30, default='price')
     required = models.BooleanField(default=True)
@@ -28,15 +35,17 @@ class Fee(models.Model):
 
 
 class Event(models.Model):
-    """Equivalent to meet up's api Event model"""
-    id = models.AutoField(primary_key=True)
-    event_meetup_id = models.IntegerField(default=0)
-    event_venue = models.ForeignKey(to=Venue, on_delete=models.CASCADE)
+    """
+    Equivalent to meet up's api Event model
+    """
+
+    event_meetup_id = models.CharField(max_length=32, default='')
+    event_venue = models.ForeignKey(to=EventVenue, on_delete=models.CASCADE)
     reservation_limit = models.IntegerField(default=100)
     headcount = models.IntegerField(default=0)
     visibility = models.CharField(max_length=10)
     waitlistcount = models.IntegerField(default=0)
-    event_fee = models.ForeignKey(to=Fee, on_delete=models.CASCADE)
+    event_fee = models.ForeignKey(to=EventFee, on_delete=models.CASCADE)
     description = models.TextField()
     event_url = models.CharField(max_length=250)
     yes_reservation_count = models.IntegerField(default=0)
@@ -48,10 +57,13 @@ class Event(models.Model):
     status = models.CharField(max_length=50, default='upcoming')
 
     def save(self, *kwargs):
-        """ Overrides default behaviour to crop the image """
+        """
+        Overrides default behaviour to crop the image
+        """
+
         super().save()
 
-        img  = Image.open(self.photo.path)
+        img = Image.open(self.photo.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
