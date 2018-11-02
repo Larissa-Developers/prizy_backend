228e293cca8f7788b3dcd751e4df569ca77829d2
ioanna <kuriakopoulosgiorgos@gmail.com>
2018-11-02 02:22:03 +0200

    Related the accounts with the events, an account is checked in with events and events are checked in from accounts, also fixed a bug causing the events listing url not to work, finally now there is an event's detail endpoint

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from events import views
 
 urlpatterns = [
-    path('events', views.EventList.as_view(), name='events'),
+    re_path(r'^events/$', views.EventList.as_view(), name='events'),
+    path('events/<pk>', views.EventDetails.as_view(), name='events-details'),
     path('meetupcom', views.Meetupcom.as_view(), name='meetupcom'),
+
 ]
